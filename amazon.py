b08e3baaa1da2dbfd3811b4859084b217f713686
thanhdat1902 <thanhdat19022001@gmail.com>
2024-11-18 16:20:06 -0800

    fix: add probability calculation for Watts model

--- amazon.py
+++ amazon.py
@@ -16,27 +16,35 @@ networkHelper = NetworkHelper()
 originalNetworkData = DataLoader("com-amazon.ungraph.txt", "Amazon").buildNetworkXGraph()
 GccNetwork = networkHelper.largestConnectedComponent(originalNetworkData)
 avgDegreeOrg = None
+averageClusteringCoefficientOrg = None
 
 def original():
     global avgDegreeOrg
     print("========================================= Original Network ======================================")
     avgDegreeOrg = networkHelper.averageDegrees(GccNetwork)
-
+    averageClusteringCoefficientOrg = networkHelper.averageClusteringCoefficient(GccNetwork)
     print("Size: ", networkHelper.getSize(GccNetwork))
-    print("Average Degree: ", networkHelper.averageDegrees(GccNetwork))
+    print("Average Degree: ", avgDegreeOrg)
     print("Average Path Length By Taking 10% Nodes In Original:" , networkHelper.averagePathLengthByPercentage(GccNetwork))
     print("Average Path Length By Taking 20% Nodes In Original:" , networkHelper.averagePathLengthByPercentage(GccNetwork, 0.2))
     print("Average Path Length By Using Random Walk Algorithm:" , networkHelper.averagePathLengthRandomWalk(GccNetwork))
-    print("Clustering Coefficient: ", networkHelper.averageClusteringCoefficient(GccNetwork))
+    print("Clustering Coefficient: ", averageClusteringCoefficientOrg)
 
 
 def watts():
-    wattsModel = networkHelper.watts_strogatz_graph(len(GccNetwork), int(avgDegreeOrg))
-    print("========================================= Watts-Strogatz ======================================")
-    print("Average Path Length By Taking 10% Nodes In Original:" , networkHelper.averagePathLengthByPercentage(wattsModel))
-    print("Average Path Length By Taking 20% Nodes In Original:" , networkHelper.averagePathLengthByPercentage(wattsModel, 0.2))
-    print("Average Path Length By Using Random Walk Algorithm:" , networkHelper.averagePathLengthRandomWalk(wattsModel))
-    print("Clustering Coefficient: " , networkHelper.averageClusteringCoefficient(wattsModel))
+    if avgDegreeOrg != None and averageClusteringCoefficientOrg != None:
+        d = int(avgDegreeOrg)
+        c0 = 3 * (d - 2) / (4 * (d - 1))
+        p = (1 - (averageClusteringCoefficientOrg / c0)) ** (1 / 3)
+        wattsModel = networkHelper.watts_strogatz_graph(len(GccNetwork), d, p)
+        print("========================================= Watts-Strogatz ======================================")
+        print("Probability p:", p)
+        print("Average Path Length By Taking 10% Nodes In Original:" , networkHelper.averagePathLengthByPercentage(wattsModel))
+        print("Average Path Length By Taking 20% Nodes In Original:" , networkHelper.averagePathLengthByPercentage(wattsModel, 0.2))
+        print("Average Path Length By Using Random Walk Algorithm:" , networkHelper.averagePathLengthRandomWalk(wattsModel))
+        print("Clustering Coefficient: " , networkHelper.averageClusteringCoefficient(wattsModel))
+    else:
+        print("Error with original network")
 
 def barabasi():
     barabasiModel = networkHelper.barabasi_albert_graph(len(GccNetwork), (int(avgDegreeOrg) // 2))
@@ -48,7 +56,6 @@ def barabasi():
 
 
 print("========================================= Amazon ======================================")
-
 original()
 barabasi()
 watts()
